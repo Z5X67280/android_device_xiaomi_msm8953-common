cae28e81f43c270d79db8923ddeb3630899be204
Keternal <Z5X67280@163.com>
2019-08-21 11:30:57 +0800

    msm8953-common: Add widevine 1.1 hal
    - Already defined in manifests.xml, but not add it in msm8953.mk, enable it.
    
    Signed-off-by: Keternal <Z5X67280@163.com>

--- msm8953.mk
+++ msm8953.mk
@@ -150,7 +150,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey
+    android.hardware.drm@1.1-service.clearkey \
+    android.hardware.drm@1.1-service.widevine
 
 # Ebtables
 PRODUCT_PACKAGES += \
